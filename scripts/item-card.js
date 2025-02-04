59c993e4b342e431d2a9f406b0fcdc43b64e6786
lizka <lizka4231@gmail.com>
2025-02-04 19:06:07 +0300

    updated item cart script

--- scripts/item-card.js
+++ scripts/item-card.js
@@ -7,6 +7,8 @@ const itemDecreaseButton = document.querySelector('.item-decrease');
 const itemIncreaseButton = document.querySelector('.item-increase');
 const buyItemButton = document.querySelector('.item-buy');
 
+const contactPanelButton = document.querySelector('.contact-panel__button');
+
 const itemsObj = {
     1: {
         name: 'Irish Stout Irish Stout',
@@ -241,10 +243,46 @@ const switchItemActionAmount = () => {
     }
 };
 
+const processForm = (e) => {
+    e.preventDefault();
+    
+    const contactInputs = document.querySelectorAll('.contact-panel__input');
+    const areInputsFilled = [...contactInputs].every(input => input.value !== '');
+
+    [...contactInputs].filter(input => input.value === '').forEach(el => {
+        el.style.border = '1px solid red';
+    });
+
+    [...contactInputs].filter(input => input.value !== '').forEach(el => {
+        el.style.border = '0';
+    });
+
+    if (areInputsFilled) {
+        
+        const contactPanel = document.querySelector('.contact-panel');
+        const contactPanelButton = document.querySelector('.contact-panel__button');
+        
+        contactPanel.classList.add('success');
+        e.target.style.cursor = 'not-allowed';
+
+        setTimeout(() => {
+            contactPanel.classList.toggle('success');
+            contactPanelButton.classList.add('hidden');
+            contactPanel.innerHTML += `<p class='success-p'>Форма успешно отправлена!</p>`;
+        }, 3000);
+    }
+    else {
+        [...contactInputs].filter(input => input.value === '').forEach(el => {
+            el.style.border = '1px solid red';
+        });
+    }
+};
+
 document.addEventListener('DOMContentLoaded', getItemAmount);
 itemDecreaseButton.addEventListener('click', itemDecrease);
 itemIncreaseButton.addEventListener('click', itemIncrease);
 [...itemDescriptionLinks].forEach(link => link.addEventListener('click', switchDescription));
+contactPanelButton.addEventListener('click', processForm)
 
 buyItemButton.addEventListener('click', () => {
     const cartObj = JSON.parse(localStorage.getItem('cart'));
