940bc370e17e67098aaa77f89579651bd0adca47
lizka <lizka4231@gmail.com>
2025-02-05 01:19:56 +0300

    finished script for filter panel for all devices

--- scripts/device-catalog.js
+++ scripts/device-catalog.js
@@ -2,6 +2,7 @@ const filterButton = document.querySelector('.filter-panel-mobile__section-main-
 const filterMobileDiv = document.querySelector('.filter-panel-mobile');
 const filterPanel = document.querySelector('.fitler-panel')
 const displayItemsButtons = document.querySelectorAll('.button-display');
+const closeFiltersButton = document.querySelector('.filter-panel-mobile__close');
 
 const toggleFilterDiv = () => {
     filterButton.classList.toggle('active');
@@ -12,13 +13,17 @@ const toggleFilterDiv = () => {
 };
 
 filterButton.addEventListener('click', toggleFilterDiv);
+closeFiltersButton.addEventListener('click', toggleFilterDiv);
 document.addEventListener('DOMContentLoaded', () => {
     if (window.innerWidth <= 450) {
         filterButton.classList.add('active');
         filterMobileDiv.classList.add('active');
         filterPanel.classList.add('hidden');
+        closeFiltersButton.classList.add('active');
     }
     else {
         filterMobileDiv.classList.add('hidden');
+        filterPanel.classList.add('active');
+        closeFiltersButton.classList.add('hidden');
     }
 });
