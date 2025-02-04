e93d6d978ce40ade09fd6c25502b4c6e47cf2491
lizka <lizka4231@gmail.com>
2025-02-05 01:59:39 +0300

    added logic for displaying 1 or 2 items in mobile catalog

--- scripts/device-catalog.js
+++ scripts/device-catalog.js
@@ -1,8 +1,12 @@
 const filterButton = document.querySelector('.filter-panel-mobile__section-main-button');
 const filterMobileDiv = document.querySelector('.filter-panel-mobile');
 const filterPanel = document.querySelector('.fitler-panel')
-const displayItemsButtons = document.querySelectorAll('.button-display');
+const displayTwoItemsButton = document.querySelector('.filter-panel-mobile__button-left');
+const displayOneItemButton = document.querySelector('.filter-panel-mobile__button-right');
 const closeFiltersButton = document.querySelector('.filter-panel-mobile__close');
+
+const cardSections = document.querySelectorAll('.card');
+const mainSection = document.querySelector('.main');
 
 const toggleFilterDiv = () => {
     filterButton.classList.toggle('active');
@@ -12,8 +16,32 @@ const toggleFilterDiv = () => {
     filterPanel.classList.toggle('hidden');
 };
 
+const displayOneItem = (e) => {
+    e.target.classList.remove('inactive');
+    e.target.classList.add('active');
+
+    displayTwoItemsButton.classList.remove('active');
+    displayTwoItemsButton.classList.add('inactive');
+
+    [...cardSections].forEach(card => card.style.width = '260px');
+    mainSection.style.padding = '0 15px';
+};
+
+const displayTwoItems = (e) => {
+    e.target.classList.remove('inactive');
+    e.target.classList.add('active');
+    
+    displayOneItemButton.classList.remove('active');
+    displayOneItemButton.classList.add('inactive');
+
+    [...cardSections].forEach(card => card.style.width = '155px');
+    mainSection.style.padding = '0';
+};
+
 filterButton.addEventListener('click', toggleFilterDiv);
 closeFiltersButton.addEventListener('click', toggleFilterDiv);
+displayOneItemButton.addEventListener('click', displayOneItem);
+displayTwoItemsButton.addEventListener('click', displayTwoItems);
 document.addEventListener('DOMContentLoaded', () => {
     if (window.innerWidth <= 450) {
         filterButton.classList.add('active');
