95fa25c5caed8c3bdabd05b9f4b91afe403f0d97
lizka <lizka4231@gmail.com>
2025-02-11 22:00:53 +0300

    added script for modal window

--- scripts/home.js
+++ scripts/home.js
@@ -1,3 +1,29 @@
+const modal = document.querySelector('.modal');
+const adultButton = document.querySelector('.button-adult');
+const underageButton = document.querySelector('.button-underage');
 const goCatalogButton = document.querySelector('.intro__button');
+const bodyChildren = document.body.children;
+const elemsToBlur = [...bodyChildren].filter(child => child !== modal && child.tagName !== 'SCRIPT');
 
+const setModalVisibility = () => {
+    if (!sessionStorage.getItem('isAdult')) {
+        [...elemsToBlur].forEach(el => el.classList.add('blurred'));
+    }
+    else {
+        modal.classList.add('hidden');
+    }
+};
+
+function modalAction(e) {
+    if (this === adultButton) {
+        modal.classList.add('hidden');
+        elemsToBlur.forEach(el => el.classList.remove('blurred'));
+
+        sessionStorage.setItem('isAdult', true)
+    }
+};
+
+document.addEventListener('DOMContentLoaded', setModalVisibility)
+adultButton.addEventListener('click', modalAction);
+underageButton.addEventListener('click', modalAction);
 goCatalogButton.addEventListener('click',()=>location.href = './catalog.html');
