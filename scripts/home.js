21ccd01cb02dcded3b9c08e9aee24083325684d5
lizka <lizka4231@gmail.com>
2025-02-17 21:49:51 +0300

    fixed positioning when modal window is on

--- scripts/home.js
+++ scripts/home.js
@@ -1,5 +1,5 @@
 const bodyChildren = document.body.children;
-const elemsToBlur = [...bodyChildren].filter(child => child.tagName !== 'SCRIPT');
+const elemsToBlur = [document.querySelector('.header'), document.querySelector('.intro'), document.querySelector('.intro-slider'), document.querySelector('.intro-dots'), document.querySelector('.intro-tip'), document.querySelector('.section-about'), document.querySelector('.footer')]
 const goCatalogButton = document.querySelector('.intro__button');
 
 const sliderBgs = document.querySelectorAll('.intro-slider__background');
@@ -21,24 +21,24 @@ const setModalVisibility = () => {
     let modal = document.querySelector('.modal') || undefined;
     if (!sessionStorage.getItem('isAdult')) {
         [...elemsToBlur].forEach(el => el.classList.add('blurred'));
-
+        
         const modal = document.createElement('div');
 
         modal.innerHTML = `
-            <div class="modal">
-                <div class="modal__wrapper">
-                    <div class="modal__inner">
-                        <p class="modal__title">Вам точно 18 лет?</p>
-                        <p class="modal__subtitle">Добро пожаловать на наш сайт Wunderbeer. Для доступа необходимо подтвердить совершеннолетний возраст.</p>
-                        <p class="modal__text">Сайт содержит информацию для лиц совершеннолетнего возраста. Сведения, размещенные на сайте, не являются рекламой, носят исключительно информационный характер, и предназначены только для личного использования.</p>
-                        <div class="modal__buttons">
-                            <button class="modal__button button-adult">Мне исполнилось 18 лет</button>
-                            <button class="modal__button button-underage">Мне ещё нет 18</button>
-                        </div>
+            <div class="modal__wrapper">
+                <div class="modal__inner">
+                    <p class="modal__title">Вам точно 18 лет?</p>
+                    <p class="modal__subtitle">Добро пожаловать на наш сайт Wunderbeer. Для доступа необходимо подтвердить совершеннолетний возраст.</p>
+                    <p class="modal__text">Сайт содержит информацию для лиц совершеннолетнего возраста. Сведения, размещенные на сайте, не являются рекламой, носят исключительно информационный характер, и предназначены только для личного использования.</p>
+                    <div class="modal__buttons">
+                        <button class="modal__button button-adult">Мне исполнилось 18 лет</button>
+                        <button class="modal__button button-underage">Мне ещё нет 18</button>
                     </div>
                 </div>
             </div>
         `;
+
+        modal.classList.add('modal');
         document.body.prepend(modal);
         setButtonEventListeners();
     }
